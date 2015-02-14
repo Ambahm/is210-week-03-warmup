c72c1e8e19af9da392a0bf196417aadc12063f5f
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-14 05:55:42 +0000

    WK 3 Warmuptask12 import functions for Decimal and fractions

---
+++ task_12.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""DIFFERNT ARITHMATIC VRIABLE TYPES  """
+import decimal
+import fractions
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
