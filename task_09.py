071fc4939f9ecbdade0e219d630a68059acb9067
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-14 04:28:24 +0000

    Wk 3 warmup T9 added a multiline doc string

--- task_09.py
+++ task_09.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+# Adding a multiline docstrings
+"""
+Though Celia tells them it's not possible, they believe her objection is made
+because both children will need to marry well in order to bring money onto the
+estate.
+All during their childhood, Richard goes into the village for lessons with the
+village priest though there's not enough money for Julia to also have lessons.
+"""
+
+# """A computer that can build the question."""
 
 THE_ANSWER_TO_EVERYTHING = 42
-
 INFINITE_IMPROBABILITY = 'browning motion'
