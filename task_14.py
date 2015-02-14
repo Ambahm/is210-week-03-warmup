298563375835538143359ea407e605351ded6688
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-14 06:28:59 +0000

    Wk 3 Warmuptask #14 Use of Operators AND and equality ==

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Logical Operator AND , == operators"""
+
+# Assigning values to new variables
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+# Checking if the result turned are correct
+INT_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
