b161a50edd38b25cbb41afa2f6d50646323428cc
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-14 06:39:42 +0000

    Wk3 Warmup T15: Concatenation Using + OPerator, converting int to string for this purose

--- task_15.py
+++ task_15.py
@@ -5,3 +5,6 @@
 
 NOT_THE_QUESTION = 'The answer to life, the universe, and everything? It\'s '
 ANSWER = 42
+
+# CONCATENATION & Converting integer to string for this purpose
+THANKS_FOR_THE_FISH = NOT_THE_QUESTION + str(ANSWER)
